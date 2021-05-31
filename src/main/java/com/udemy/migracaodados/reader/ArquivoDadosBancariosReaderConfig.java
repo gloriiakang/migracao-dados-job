193cddf080b312d950e9fa3dd49453027f32fc20
Gloria <gck_92@yahoo.com>
2021-05-31 10:47:03 -0300

    Criação dos Readers

---
+++ src/main/java/com/udemy/migracaodados/reader/ArquivoDadosBancariosReaderConfig.java
@@ -0,0 +1,27 @@
+package com.udemy.migracaodados.reader;
+
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+import com.udemy.migracaodados.dominio.DadosBancarios;
+import com.udemy.migracaodados.dominio.Pessoa;
+
+@Configuration
+public class ArquivoDadosBancariosReaderConfig {
+	
+	@Bean
+	public FlatFileItemReader<DadosBancarios> dadosBancariosReader() {
+		return new FlatFileItemReaderBuilder<DadosBancarios>()
+				.name("dadosBancariosReader")
+				.resource(new FileSystemResource("files/dados_bancarios.csv"))
+				.delimited()
+				.names("pessoaId", "agencia", "conta", "banco", "id")
+				.addComment("--")
+				.targetType(DadosBancarios.class)
+				.build();
+	}
+	
+}
